8cab8be07f0f16707d68a6e0a2d4486ae7a79cb7
sam <verlaansam@gmail.com>
2024-05-08 11:15:50 +0200

    add product control to admin page

--- app/Http/Controllers/user_table_controller.php
+++ app/Http/Controllers/user_table_controller.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\models\User;
+use App\models\product_table;
 
 class user_table_controller extends Controller
 {
     public function admin(){
-        return view('admin', ['users' => User::all()]);
+        $data = ['product' => product_table::all(),
+                 'users' => User::all()];
+        return view('admin', $data);
     }
 
     public function delete_user($id){
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,13 +9,13 @@
             <h1 class="text-amber-700 text-3xl font-medium pl-5 pt-3">Users</h1>
             <article class=" h-fit w-screen flex flex-wrap">
                 @if ($users->isEmpty())
-                    <section class="flex items-center justify-center content-center h-[400px] w-screen">
+                    <section class="flex items-center justify-center content-center h-[200px] w-screen">
                         <h1 class="text-orange-200 text-3xl">Looks like no one's home :(</h1>
                     </section> 
                 @endif
                 @foreach ($users as $user)
-                <section class="flex items-start justify-center h-[400px] w-[350px]">
-                    <article class="bg-gray-200 w-[90%] h-[20%] rounded-xl shadow-lg p-4">
+                <section class="flex items-start justify-center h-[75px] w-[350px]">
+                    <article class="bg-gray-200 w-[90%] h-[100%] rounded-xl shadow-lg p-4">
                         <p>{{$user->name}}</p>
                         <p>{{$user->email}}</p>
                         <form class="relative bottom-[97%] left-[88%]" method="POST" action="{{ route('delete_user', $user->id)}}"">
@@ -28,6 +28,35 @@
                     </article>
                 </section>
                 @endforeach 
+            </article>
+        </div>
+        <div class="flex justify-center flex-col">
+            <h1 class="text-amber-700 text-3xl font-medium pl-5 pt-3">Products</h1>
+            <article class=" h-fit w-screen flex flex-wrap">
+                @if ($product->isEmpty())
+                    <section class="flex items-center justify-center content-center h-[400px] w-screen">
+                        <h1 class="text-orange-200 text-3xl">Looks like no one's home :(</h1>
+                    </section> 
+                @endif
+                @foreach ($product as $product_table)
+                <section class="flex items-center justify-center h-[400px] w-[350px]">
+                    <article class="bg-gray-200 w-[90%] h-[90%] rounded-xl shadow-lg">
+                        <p class="w-full h-[80%] bg-green-500 rounded-t-xl">{{ $product_table->status}}</p>
+                        <div class=" h-1/6 grid grid-cols-2 m-1">
+                            <h1 class="text-4xl text-amber-700">{{ $product_table->name}}</h1>
+                            <p class="col-start-1 text-gray-600">{{ $product_table->location}}</p>
+                            <p class=" col-start-2 justify-self-end text-gray-600  w-[200px]">{{ $product_table->availableFrom}} / {{ $product_table->availableTill}}</p> 
+                        </div>
+                        <form class="relative bottom-[97%] left-[88%]" method="POST" action="{{ route('delete_product', $product_table->id)}}"">
+                            {{ csrf_field() }}
+                            <button ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                            </button>
+                        </form>
+                    </article>
+                </section>
+                @endforeach
             </article>
         </div>
     </div>
